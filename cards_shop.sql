ec215e2d114975dc2bbdee318d227980a4d1c927
margin-right <i.ruden@mail.ru>
2022-01-24 02:52:17 +1000

    add correct database

--- cards_shop.sql
+++ cards_shop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Янв 12 2022 г., 20:58
+-- Время создания: Янв 23 2022 г., 19:49
 -- Версия сервера: 8.0.24
 -- Версия PHP: 7.1.33
 
@@ -64,7 +64,7 @@ CREATE TABLE `cards` (
   `chipCompany` varchar(50) NOT NULL,
   `vendor` varchar(50) NOT NULL,
   `spec` varchar(20) NOT NULL DEFAULT 'for-game'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Дамп данных таблицы `cards`
@@ -80,6 +80,42 @@ INSERT INTO `cards` (`id`, `name`, `linkName`, `cost`, `img`, `techprocess`, `me
 (7, 'KFA2 GeForce GT 710', 'Kfa2GeForceGT710', 4499, 'Kfa2GeForceGT710', 28, 2, 64, 'да', 24, '-', 2014, 'GDDR3', 12, 'Kepler', 954, 976, 192, 'нет', 'DVI-D, HDMI, VGA (D-Sub)', '4096x2160', 3, 'нет', 'нет', 19, 300, 'пассивное', 0, 'да', 1, 172, 'документация', 'нет', 'Nvidia', 'KFA2', 'for-office'),
 (8, 'PNY Quadro T600', 'PnyQuadroT600', 20999, 'PnyQuadroT600', 12, 4, 128, 'да', 36, 'PNY Quadro', 2021, 'GDDR6', 160, 'Turing', 735, 1330, 640, 'нет', 'Mini DisplayPort x4', '7680x4320', 4, 'нет', 'нет', 40, 300, 'активное воздушное', 1, 'да', 1, 156, 'переходник miniDisplayPort - DisplayPort x4, документация, низкопрофильная планка', 'нет', 'Nvidia', 'PNY', 'prof');
 
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `orders`
+--
+
+CREATE TABLE `orders` (
+  `id` int NOT NULL,
+  `token` varchar(255) NOT NULL,
+  `products` varchar(255) NOT NULL,
+  `phone` varchar(10) NOT NULL,
+  `adres` text NOT NULL,
+  `cost` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
+--
+
+CREATE TABLE `users` (
+  `id` int NOT NULL,
+  `login` varchar(16) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `token` varchar(255) NOT NULL,
+  `cart` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Дамп данных таблицы `users`
+--
+
+INSERT INTO `users` (`id`, `login`, `password`, `token`, `cart`) VALUES
+(7, 'admin', '$2y$10$.oRm654F8W9Q.1Db..OyzuQuelLVD5BCAxtTQvr1ZIvX3fwOd30lG', '083dc2e5bb094a40ce382f3407a5a3cbaab29f7f', '');
+
 --
 -- Индексы сохранённых таблиц
 --
@@ -91,6 +127,20 @@ ALTER TABLE `cards`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Индексы таблицы `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `token` (`token`);
+
+--
+-- Индексы таблицы `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `token` (`token`);
+
+--
 -- AUTO_INCREMENT для сохранённых таблиц
 --
 
@@ -99,6 +149,28 @@ ALTER TABLE `cards`
 --
 ALTER TABLE `cards`
   MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT для таблицы `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT для таблицы `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- Ограничения внешнего ключа сохраненных таблиц
+--
+
+--
+-- Ограничения внешнего ключа таблицы `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`token`) REFERENCES `users` (`token`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
